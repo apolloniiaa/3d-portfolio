58a4cbc7a7b23ecdf5f413c8838876570ad93fc7
apolloniiaa <apoll93@hotmail.com>
2023-06-05 22:05:21 +0200

    issue fixed

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -28,7 +28,7 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+        className='lg:bg-tertiary  md:bg-tertiary md:p-5  lg:p-5  rounded-2xl sm:w-[360px] w-full'
       >
         <div className='relative w-full h-[230px]'>
           <img
